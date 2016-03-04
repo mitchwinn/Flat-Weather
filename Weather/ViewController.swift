7623962b6c763c1ebab9915efe5faa29cb88d253
mitchwinn <mwinn@accela.com>
2016-03-03 16:33:53 -0800

    Added a custom animation to the refresh button to spin while data is being fetched so that the user knows that network activity is happening.

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var currentForecastSummary: UILabel!
     @IBOutlet weak var nextHourTempLabel: UILabel!
     @IBOutlet weak var nextHourWeatherIcon: UIImageView!
+    @IBOutlet weak var refreshButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,18 +53,52 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             forcastIOApi.getCurrentLocationWeatherSummary(lastLocation!, completionBlock: {
                 (let forecastSummary: ForecastSummary) in
                 // Update UI with forecastSummary data.
-                self.updateWeatherView(Int(forecastSummary.currentTemperature), currentWeatherIcon: forecastSummary.weatherIconName, currentForecastSummary: forecastSummary.forecastSummary, nextHourTemp: Int(forecastSummary.nextHourTemperature), nextHourWeatherIcon: forecastSummary.nextHourWeatherIconName)
+                self.updateWeatherView(Int(forecastSummary.currentTemperature),
+                                           currentWeatherIcon: forecastSummary.weatherIconName,
+                                           currentForecastSummary: forecastSummary.forecastSummary,
+                                           nextHourTemp: Int(forecastSummary.nextHourTemperature),
+                                           nextHourWeatherIcon: forecastSummary.nextHourWeatherIconName)
+                self.stopSpinningAnnimation()
             })
             manager.stopUpdatingLocation()
         }
     }
     
-    func updateWeatherView(currentTemp: Int, currentWeatherIcon: String, currentForecastSummary: String,
-        nextHourTemp: Int, nextHourWeatherIcon: String) {
+    func startSpinningAnnimation() {
+        // create a spin animation
+        let spinAnimation = CABasicAnimation()
+        // starts from 0
+        spinAnimation.fromValue = 0
+        // goes to 360 ( 2 * π )
+        spinAnimation.toValue = M_PI*2
+        // define how long it will take to complete a 360
+        spinAnimation.duration = 1
+        // make it spin infinitely
+        spinAnimation.repeatCount = Float.infinity
+        // do not remove when completed
+        spinAnimation.removedOnCompletion = false
+        // specify the fill mode
+        spinAnimation.fillMode = kCAFillModeForwards
+        // and the animation acceleration
+        spinAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        // add the animation to the button layer
+        self.refreshButton.layer.addAnimation(spinAnimation, forKey: "transform.rotation.z")
+    }
+    
+    func stopSpinningAnnimation() {
+        // remove the animation
+        self.refreshButton.layer.removeAllAnimations()
+    }
+    
+    func updateWeatherView(currentTemp: Int, currentWeatherIcon: String, currentForecastSummary: String, nextHourTemp: Int, nextHourWeatherIcon: String) {
+        let weatherIconImage = UIImage(named: currentWeatherIcon)
         self.currentTempLabel.text = "\(currentTemp)F"
-        self.weatherIconImageView.image = UIImage(named: currentWeatherIcon)
-        // x 96 y 109
-        self.weatherIconImageView.frame = CGRectMake((self.cardView.frame.width / 2) - ((UIImage(named: currentWeatherIcon)?.size.width)! / 2), 109, (UIImage(named: currentWeatherIcon)?.size.width)!, (UIImage(named: currentWeatherIcon)?.size.height)!)
+        self.weatherIconImageView.image = weatherIconImage
+        // Image should be around x: ~96 y: 109
+        self.weatherIconImageView.frame = CGRectMake((self.cardView.frame.width / 2) - (weatherIconImage!.size.width / 2),
+                                                     109,
+                                                     weatherIconImage!.size.width,
+                                                     weatherIconImage!.size.height)
         self.currentForecastSummary.text = currentForecastSummary
         self.nextHourTempLabel.text = "\(nextHourTemp)F"
         self.nextHourWeatherIcon.image = UIImage(named: nextHourWeatherIcon)
@@ -71,13 +106,16 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         switch currentWeatherIcon {
         case "ClearDay",
              "PartlyCloudyDay":
+            // Burnt Orange
             self.cardView.backgroundColor = UIColor(red: 255/255, green: 137/255, blue: 0.0, alpha: 1.0)
         default:
+            // Rainy Blue
             self.cardView.backgroundColor = UIColor(red: 131/255, green: 177/255, blue: 196/255, alpha: 1.0)
         }
     }
     
     @IBAction func refreshButtonTapped(sender: AnyObject) {
+        self.startSpinningAnnimation()
         self.locationManager.startUpdatingLocation()
     }
 }
